3b680e5f6f5fdda362fdb8b0c506e490f42c4445
joonb14 <joonb14@gmail.com>
2022-08-23 23:48:50 +0900

    [refactor] text-animation 적용을 위한 rebase

---
+++ src/TextAnimation.jsx
@@ -0,0 +1,48 @@
+import React, {useEffect} from 'react';
+
+import './assets/reset.css';
+import './assets/textanimation.css';
+
+import { ReactComponent as TextSVG } from './assets/textanimation.svg';
+
+/* @bravoyourlif */
+function HeaderContent(props) {
+
+  return (
+    <div className="HeaderContent">
+      <TextSVG></TextSVG>
+    </div>
+  );
+}
+
+function ClipSVG(props) {
+    const bgGroup = document.querySelector('.svg-bg')
+    const bgPaths = bgGroup.querySelectorAll('path')
+    Animate(bgPaths)
+
+    const mainGroup = document.querySelector('.svg-main')
+    const mainPaths = mainGroup.querySelectorAll('path')
+    Animate(mainPaths)
+}
+
+function Animate(paths) {
+  paths.forEach((path, index) => {
+    const length = path.getTotalLength()
+    path.style.setProperty('--length', length)
+    path.style.setProperty('--delay', index * 200 + 'ms')
+    path.style.setProperty('--duration', length * 100 + 'ms')
+  }) 
+}
+
+export default function Header(props) {
+
+    useEffect(() => {
+      ClipSVG();
+    }) 
+
+    return (
+      <div className="Header">
+        <HeaderContent></HeaderContent>
+      </div>
+    );
+}
